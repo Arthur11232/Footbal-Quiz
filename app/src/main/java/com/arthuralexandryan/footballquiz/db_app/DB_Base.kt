5e681203a3aec4dc5ab9b6cd6ccf009a3cf649ae
Artur Aleksandryan <arthuralexandryan@gmail.com>
2026-03-23 13:11:04 +0400

    Convert BaseActivity and MainActivity to Kotlin

--- app/src/main/java/com/arthuralexandryan/footballquiz/db_app/DB_Base.kt
+++ app/src/main/java/com/arthuralexandryan/footballquiz/db_app/DB_Base.kt
@@ -1,20 +1,12 @@
-package com.arthuralexandryan.footballquiz.db_app;
+package com.arthuralexandryan.footballquiz.db_app
 
-public interface DB_Base {
-    int getId();
-    void setId(int id);
-    String getQuestion();
-    void setQuestion(String question);
-    String getAnswer_A();
-    void setAnswer_A(String answer_A);
-    String getAnswer_B();
-    void setAnswer_B(String answer_B);
-    String getAnswer_C();
-    void setAnswer_C(String answer_C);
-    String getAnswer_D();
-    void setAnswer_D(String answer_D);
-    String getRight_answer();
-    void setRight_answer(String right_answer);
-    boolean isAnswered();
-    void setAnswered(boolean answered);
+interface DB_Base {
+    var id: Int
+    var question: String?
+    var answer_A: String?
+    var answer_B: String?
+    var answer_C: String?
+    var answer_D: String?
+    var right_answer: String?
+    var isAnswered: Boolean
 }
